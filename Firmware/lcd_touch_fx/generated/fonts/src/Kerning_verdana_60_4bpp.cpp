6cc1c9d230756fe879b2d53bef88e857f41fd8bd
Aitoubella <ait.oubella.marouane@gmail.com>
2023-10-03 06:06:22 -0300

    LCD: main frame in progress

---
+++ Firmware/lcd_touch_fx/generated/fonts/src/Kerning_verdana_60_4bpp.cpp
@@ -0,0 +1,6 @@
+#include <touchgfx/Font.hpp>
+
+FONT_KERNING_LOCATION_FLASH_PRAGMA
+KEEP extern const touchgfx::KerningNode kerning_verdana_60_4bpp[] FONT_KERNING_LOCATION_FLASH_ATTRIBUTE = {
+    { 0x0043, -1 },  // (First char = [0x0043, C], Second char = [0x002D, -], Kerning dist = -1)
+};
